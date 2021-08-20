4208013a56cf17f799c57935034ec27d8cd17fc6
Osita Okonkwo <mrchrisok@hotmail.com>
2021-08-20 06:23:22 -0500

    updates/fixes .. still broken however

--- SecEdgarMiner/Logging/Information.cs
+++ SecEdgarMiner/Logging/Information.cs
@@ -22,7 +22,12 @@ namespace SecEdgarMiner.Logging
 	  }
 	  public static string Form4InsiderBuyingShortAlertEmail(Form4Info form4Info)
 	  {
-		 return $"{form4Info.IssuerTradingSymbol} \n{form4Info.HtmlUrl}";
+		 var chartHref = $"https://stockcharts.com/h-sc/ui?s={form4Info.IssuerTradingSymbol}";
+		 var tickerLink = $"<a href ='{chartHref}'>{form4Info.IssuerTradingSymbol}</a>";
+
+		 //return $"{form4Info.IssuerTradingSymbol} \n{form4Info.HtmlUrl}";
+
+		 return $"{tickerLink} \n{form4Info.HtmlUrl}";
 	  }
    }
 }
--- SecEdgarMiner/MailerOptions.cs
+++ SecEdgarMiner/MailerOptions.cs
@@ -5,9 +5,20 @@
 	  public string AlertFilePath { get; set; }
 	  public string DerivativeAlertFileName { get; set; }
 	  public string NonDerivativeAlertFileName { get; set; }
+	  public string SendGridApiUri { get; set; }
 	  public string SendGridApiKey { get; set; }
 
-	  public string DerivativeAlertFileUri => $"{AlertFilePath}{DerivativeAlertFileName}";
-	  public string NonDerivativeAlertFileUri => $"{AlertFilePath}{NonDerivativeAlertFileName}";
+	  public string DerivativeAlertFileUri => GetAlertFileUri(DerivativeAlertFileName);
+	  public string NonDerivativeAlertFileUri => GetAlertFileUri(NonDerivativeAlertFileName);
+
+	  private string GetAlertFileUri(string fileName)
+	  {
+		 if (string.IsNullOrWhiteSpace(AlertFilePath))
+		 {
+			AlertFilePath = System.IO.Directory.GetCurrentDirectory();
+		 }
+
+		 return $"{AlertFilePath}\\{fileName}";
+	  }
    }
 }
