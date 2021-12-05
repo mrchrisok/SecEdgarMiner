d9e30c6e72758a18c48c8e192ce58ca399c5b5ce
Osita Okonkwo <mrchrisok@hotmail.com>
2021-12-05 13:19:36 -0600

    refactor f4eng

--- SecEdgarMiner/Domain/Engines/Form4Engine.cs
+++ SecEdgarMiner/Domain/Engines/Form4Engine.cs
@@ -12,202 +12,205 @@ using System.Threading.Tasks;
 
 namespace SecEdgarMiner.Domain.Engines
 {
-   public class Form4Engine : IForm4Engine
-   {
-	  public Form4Engine(IHttpClientFactory httpClientFactory, MarketMinerContext dbContext, ILogger<Form4Engine> logger)
-	  {
-		 _client = httpClientFactory.CreateClient("SecEdgarMinerClient");
-		 _dbContext = dbContext;
-		 _logger = logger;
-	  }
+    public class Form4Engine : IForm4Engine
+    {
+        public Form4Engine(IHttpClientFactory httpClientFactory, MarketMinerContext dbContext, ILogger<Form4Engine> logger)
+        {
+            _client = httpClientFactory.CreateClient("SecEdgarMinerClient");
+            _dbContext = dbContext;
+            _logger = logger;
+        }
 
-	  private readonly MarketMinerContext _dbContext;
-	  private readonly HttpClient _client;
-	  private readonly ILogger<Form4Engine> _logger;
+        private readonly MarketMinerContext _dbContext;
+        private readonly HttpClient _client;
+        private readonly ILogger<Form4Engine> _logger;
 
-	  private static readonly string[] _stockIndications = new string[] { "STOCK", "COMMON", "SHARES" };
+        private static readonly string[] _stockIndications = new string[] { "STOCK", "COMMON", "SHARES" };
 
-	  public async Task<Form4InfoModel> GetInsiderBuyingForm4InfoAsync(Form4InfoModel form4Info)
-	  {
-		 var form4Statement = await GetForm4StatementAsync(form4Info);
+        public async Task<Form4InfoModel> GetInsiderBuyingForm4InfoAsync(Form4InfoModel form4Info)
+        {
+            var form4Statement = await GetForm4StatementAsync(form4Info);
 
-		 if (form4Statement == null)
-		 {
-			return null;
-		 }
+            if (form4Statement == null)
+            {
+                return null;
+            }
 
-		 var derivativeInsiderTransactions = GetDerivativeInsiderBuyingTransactions(form4Info, form4Statement);
-		 var nonDerivativeInsiderBuyingTransactions = GetNonDerivativeInsiderBuyingTransactions(form4Info, form4Statement);
+            var derivativeInsiderTransactions = GetDerivativeInsiderBuyingTransactions(form4Info, form4Statement);
+            var nonDerivativeInsiderBuyingTransactions = GetNonDerivativeInsiderBuyingTransactions(form4Info, form4Statement);
 
-		 if (derivativeInsiderTransactions.Count() == 0 && nonDerivativeInsiderBuyingTransactions.Count() == 0)
-		 {
-			return null;
-		 }
+            if (derivativeInsiderTransactions.Count() == 0 && nonDerivativeInsiderBuyingTransactions.Count() == 0)
+            {
+                return null;
+            }
 
-		 var insiderBuyingform4Info = new Form4InfoModel
-		 {
-			PeriodOfReport = form4Statement.PeriodOfReport,
-			OwnerName = form4Statement.OwnerName,
-			OwnerCik = form4Statement.OwnerCik,
-			OwnerIsOfficer = form4Statement.OwnerIsOfficer,
-			OwnerOfficerTitle = form4Statement.OwnerOfficerTitle,
-			IssuerName = form4Statement.IssuerName,
-			IssuerTradingSymbol = form4Statement.IssuerTradingSymbol,
-			IssuerCik = form4Statement.IssuerCik,
-			//
-			XmlUrl = form4Info.XmlUrl,
-			HtmlUrl = form4Info.HtmlUrl
-		 };
+            var insiderBuyingform4Info = new Form4InfoModel
+            {
+                PeriodOfReport = form4Statement.PeriodOfReport,
+                OwnerName = form4Statement.OwnerName,
+                OwnerCik = form4Statement.OwnerCik,
+                OwnerIsOfficer = form4Statement.OwnerIsOfficer,
+                OwnerOfficerTitle = form4Statement.OwnerOfficerTitle,
+                IssuerName = form4Statement.IssuerName,
+                IssuerTradingSymbol = form4Statement.IssuerTradingSymbol,
+                IssuerCik = form4Statement.IssuerCik,
+                //
+                XmlUrl = form4Info.XmlUrl,
+                HtmlUrl = form4Info.HtmlUrl
+            };
 
-		 if (derivativeInsiderTransactions.Count() != 0)
-		 {
-			insiderBuyingform4Info.AddInsiderBuyingAlertType(InsiderBuyingAlertType.Derivative);
-		 }
-		 if (nonDerivativeInsiderBuyingTransactions.Count() != 0)
-		 {
-			insiderBuyingform4Info.AddInsiderBuyingAlertType(InsiderBuyingAlertType.NonDerivative);
-		 }
+            if (derivativeInsiderTransactions.Count() != 0)
+            {
+                insiderBuyingform4Info.AddInsiderBuyingAlertType(InsiderBuyingAlertType.Derivative);
+            }
+            if (nonDerivativeInsiderBuyingTransactions.Count() != 0)
+            {
+                insiderBuyingform4Info.AddInsiderBuyingAlertType(InsiderBuyingAlertType.NonDerivative);
+            }
 
-		 _logger.LogInformation(Information.Form4InsiderBuyingFoundInIssuer(insiderBuyingform4Info));
+            _logger.LogInformation(Information.Form4InsiderBuyingFoundInIssuer(insiderBuyingform4Info));
 
-		 await SaveForm4InfoAsync(insiderBuyingform4Info);
+            await SaveForm4InfoAsync(insiderBuyingform4Info);
 
-		 return insiderBuyingform4Info;
-	  }
+            return insiderBuyingform4Info;
+        }
 
-	  public async Task<StatementOfBeneficialOwnership> GetForm4StatementAsync(Form4InfoModel form4Info)
-	  {
-		 string form4XmlString;
-		 try
-		 {
-			var response = await _client.GetAsync(form4Info.XmlUrl);
-			var form4XmlResponse = await HttpHelper.GetResponseMessageAsync(response);
-			form4XmlString = Uri.UnescapeDataString(form4XmlResponse);
-		 }
-		 catch (Exception ex)
-		 {
-			_logger.LogError($"HttpGet error occured for statement: {form4Info.XmlUrl}. \nMessage: {ex.Message}");
-			return null;
-		 }
+        public async Task<StatementOfBeneficialOwnership> GetForm4StatementAsync(Form4InfoModel form4Info)
+        {
+            string form4XmlString;
+            try
+            {
+                var response = await _client.GetAsync(form4Info.XmlUrl);
+                var form4XmlResponse = await HttpHelper.GetResponseMessageAsync(response);
+                form4XmlString = Uri.UnescapeDataString(form4XmlResponse);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"HttpGet error occured for statement: {form4Info.XmlUrl}. \nMessage: {ex.Message}");
+                return null;
+            }
 
-		 StatementOfBeneficialOwnership form4Statement;
-		 try
-		 {
-			form4Statement = StatementOfBeneficialOwnership.ParseXml(form4XmlString);
-		 }
-		 catch (Exception ex)
-		 {
-			_logger.LogError($"XML error occured for statement: {form4Info.XmlUrl}. \nMessage: {ex.Message}");
-			return null;
-		 }
+            StatementOfBeneficialOwnership form4Statement;
+            try
+            {
+                form4Statement = StatementOfBeneficialOwnership.ParseXml(form4XmlString);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"XML error occured for statement: {form4Info.XmlUrl}. \nMessage: {ex.Message}");
+                return null;
+            }
 
-		 return form4Statement;
-	  }
+            return form4Statement;
+        }
 
-	  /// <summary>
-	  /// 
-	  /// </summary>
-	  /// <param name="form4Info"></param>
-	  /// <param name="form4Statement"></param>
-	  /// <returns>Returns a list of NonDerivativeTransaction objects</returns>
-	  private IEnumerable<NonDerivativeTransaction> GetNonDerivativeInsiderBuyingTransactions(Form4InfoModel form4Info, StatementOfBeneficialOwnership form4Statement)
-	  {
-		 static bool transactionPriceNearLastPrice(float? transactionPrice)
-		 {
-			return true;
+        public Task<IEnumerable<SecurityTransaction>> GetInsiderBuyingTransactionsAsync(Form4InfoModel form4Info)
+        {
+            throw new NotImplementedException();
+        }
 
-			//var stockTicker = form4Statement.IssuerTradingSymbol;
-			//var stockLastTradingPrice = await GetStockLastTradingPrice(stockTicker);
+        #region utilities
 
-			//var lastMarketPricePlus10Percent = stockLastTradingPrice * 1.10;
-			//var lastMarketPriceMinus10Percent = stockLastTradingPrice * 0.90;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="form4Info"></param>
+        /// <param name="form4Statement"></param>
+        /// <returns>Returns a list of NonDerivativeTransaction objects</returns>
+        private IEnumerable<NonDerivativeTransaction> GetNonDerivativeInsiderBuyingTransactions(Form4InfoModel form4Info, StatementOfBeneficialOwnership form4Statement)
+        {
+            IEnumerable<NonDerivativeTransaction> insiderBuyingTransactions = null;
+            try
+            {
+                insiderBuyingTransactions = form4Statement?.NonDerivativeTransactions?
+                   .Where(transaction =>
+                      _stockIndications.Any(indication => transaction.SecurityTitle.ToUpperInvariant().Contains(indication))
+                         && IsOpenMarketOrPrivatePurchase(transaction)
+                         && transaction.TransactionQuantity.GetValueOrDefault() > 1000.0
+                         && transaction.AcquiredOrDisposed.GetValueOrDefault(AcquiredDisposed.Disposed) == AcquiredDisposed.Acquired
+                         && transaction.TransactionPricePerSecurity.GetValueOrDefault() > 5.00
+                         && TransactionPriceNearLastPrice(transaction.TransactionPricePerSecurity)
+                      //&& transaction.DirectOrIndirectOwnership == OwnershipNature.Direct
+                      );
+            }
+            catch (Exception)
+            {
+                _logger.LogError($"LINQ error occured for statement: {form4Info.XmlUrl}");
+            }
 
-			//return transactionPrice >= lastMarketPriceMinus10Percent
-			//   && transactionPrice <= lastMarketPricePlus10Percent;
-		 }
+            return insiderBuyingTransactions ?? new List<NonDerivativeTransaction>();
+        }
 
-		 IEnumerable<NonDerivativeTransaction> insiderBuyingTransactions = null;
-		 try
-		 {
-			insiderBuyingTransactions = form4Statement?.NonDerivativeTransactions?
-			   .Where(transaction =>
-				  _stockIndications.Any(indication => transaction.SecurityTitle.ToUpperInvariant().Contains(indication))
-					 && IsOpenMarketOrPrivatePurchase(transaction)
-					 && transaction.TransactionQuantity.GetValueOrDefault() > 1000.0
-					 && transaction.AcquiredOrDisposed.GetValueOrDefault(AcquiredDisposed.Disposed) == AcquiredDisposed.Acquired
-					 && transaction.TransactionPricePerSecurity.GetValueOrDefault() > 5.00
-					 && transactionPriceNearLastPrice(transaction.TransactionPricePerSecurity)
-				  //&& transaction.DirectOrIndirectOwnership == OwnershipNature.Direct
-				  );
-		 }
-		 catch (Exception)
-		 {
-			_logger.LogError($"LINQ error occured for statement: {form4Info.XmlUrl}");
-		 }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="form4Info"></param>
+        /// <param name="form4Statement"></param>
+        /// <returns>Returns a list of DerivativeTransaction objects</returns>
+        private IEnumerable<DerivativeTransaction> GetDerivativeInsiderBuyingTransactions(Form4InfoModel form4Info, StatementOfBeneficialOwnership form4Statement)
+        {
+            static bool transactionPriceNearLastPrice(float? transactionPrice)
+            {
+                return true;
+            }
 
-		 return insiderBuyingTransactions ?? new List<NonDerivativeTransaction>();
-	  }
+            IEnumerable<DerivativeTransaction> insiderBuyingTransactions = null;
+            try
+            {
+                insiderBuyingTransactions = form4Statement?.DerivativeTransactions?
+                   .Where(transaction =>
+                         //_stockIndications.Any(indication => transaction.SecurityTitle.ToUpperInvariant().Contains(indication))
+                         IsOpenMarketOrPrivatePurchase(transaction)
+                         && transaction.TransactionQuantity.GetValueOrDefault() > 1000.0
+                         && transaction.AcquiredOrDisposed.GetValueOrDefault(AcquiredDisposed.Disposed) == AcquiredDisposed.Acquired
+                         && transaction.TransactionPricePerSecurity.GetValueOrDefault() > 0
+                         && transactionPriceNearLastPrice(transaction.TransactionPricePerSecurity)
+                      );
+            }
+            catch (Exception)
+            {
+                _logger.LogError($"LINQ error occured for statement: {form4Info.XmlUrl}");
+            }
 
-	  /// <summary>
-	  /// 
-	  /// </summary>
-	  /// <param name="form4Info"></param>
-	  /// <param name="form4Statement"></param>
-	  /// <returns>Returns a list of DerivativeTransaction objects</returns>
-	  private IEnumerable<DerivativeTransaction> GetDerivativeInsiderBuyingTransactions(Form4InfoModel form4Info, StatementOfBeneficialOwnership form4Statement)
-	  {
-		 static bool transactionPriceNearLastPrice(float? transactionPrice)
-		 {
-			return true;
-		 }
+            return insiderBuyingTransactions ?? new List<DerivativeTransaction>();
+        }
 
-		 IEnumerable<DerivativeTransaction> insiderBuyingTransactions = null;
-		 try
-		 {
-			insiderBuyingTransactions = form4Statement?.DerivativeTransactions?
-			   .Where(transaction =>
-					 //_stockIndications.Any(indication => transaction.SecurityTitle.ToUpperInvariant().Contains(indication))
-					 IsOpenMarketOrPrivatePurchase(transaction)
-					 && transaction.TransactionQuantity.GetValueOrDefault() > 1000.0
-					 && transaction.AcquiredOrDisposed.GetValueOrDefault(AcquiredDisposed.Disposed) == AcquiredDisposed.Acquired
-					 && transaction.TransactionPricePerSecurity.GetValueOrDefault() > 0
-					 && transactionPriceNearLastPrice(transaction.TransactionPricePerSecurity)
-				  //&& transaction.DirectOrIndirectOwnership == OwnershipNature.Direct
-				  );
-		 }
-		 catch (Exception)
-		 {
-			_logger.LogError($"LINQ error occured for statement: {form4Info.XmlUrl}");
-		 }
+        private static bool TransactionPriceNearLastPrice(float? transactionPrice)
+        {
+            return true;
 
-		 return insiderBuyingTransactions ?? new List<DerivativeTransaction>();
-	  }
+            //var stockTicker = form4Statement.IssuerTradingSymbol;
+            //var stockLastTradingPrice = await GetStockLastTradingPrice(stockTicker);
 
-	  private bool IsOpenMarketOrPrivatePurchase(SecurityTransaction transaction)
-	  {
-		 return transaction.TransactionCode.GetValueOrDefault(TransactionType.OpenMarketOrPrivateSale) == TransactionType.OpenMarketOrPrivatePurchase;
-	  }
+            //var lastMarketPricePlus10Percent = stockLastTradingPrice * 1.10;
+            //var lastMarketPriceMinus10Percent = stockLastTradingPrice * 0.90;
 
-	  private Task<double> GetStockLastTradingPriceAsync(string tradingSymbol)
-	  {
-		 return Task.FromResult(5.00);
-	  }
+            //return transactionPrice >= lastMarketPriceMinus10Percent
+            //   && transactionPrice <= lastMarketPricePlus10Percent;
+        }
 
-	  public Task<IEnumerable<SecurityTransaction>> GetInsiderBuyingTransactionsAsync(Form4InfoModel form4Info)
-	  {
-		 throw new NotImplementedException();
-	  }
+        private bool IsOpenMarketOrPrivatePurchase(SecurityTransaction transaction)
+        {
+            return transaction.TransactionCode.GetValueOrDefault(TransactionType.OpenMarketOrPrivateSale) == TransactionType.OpenMarketOrPrivatePurchase;
+        }
 
-	  private async Task SaveForm4InfoAsync(Form4InfoModel form4Info)
-	  {
-		 try
-		 {
-			await _dbContext.CreateAsync(form4Info);
-		 }
-		 catch (Exception ex)
-		 {
-			_logger.LogError($"Db Save error occured for statement: {form4Info.XmlUrl}. Error Msg: {ex.InnerException.Message}");
-		 }
-	  }
-   }
+        private Task<double> GetInstrumentLastTradingPriceAsync(string tradingSymbol)
+        {
+            return Task.FromResult(5.00);
+        }
+
+        #endregion
+
+        private async Task SaveForm4InfoAsync(Form4InfoModel form4Info)
+        {
+            try
+            {
+                await _dbContext.CreateAsync(form4Info);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Db Save error occured for statement: {form4Info.XmlUrl}. Error Msg: {ex.InnerException.Message}");
+            }
+        }
+    }
 }
